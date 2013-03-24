fb52a797fcd5e6d46a9880bb1f6e3035183d9308
Shayne Huddleston <shayne.huddleston@oregonstate.edu>
2013-03-24 10:04:52 -0700

    refactoring and cleaning up unecessary code

--- zenoss.py
+++ zenoss.py
@@ -41,9 +41,8 @@ class Zenoss():
         if dir: data['dir'] = dir
 
         # Cheasy way to fix broken Zenoss 4 json implementation
-        data['keys'] = ['eventState', 'severity', 'component', 'eventClass',
-                        'summary', 'firstTime', 'lastTime', 'count', 'evid',
-                        'message', 'DeviceClass', 'Location', 'Systems', 'device']
+        data['keys'] = ['eventState', 'severity', 'component',
+                        'firstTime', 'lastTime', 'evid', 'device']
         data['params'] = filter
 
         return self.request([data])['result']
@@ -62,56 +61,31 @@ def main():
     parser.add_option("--dir", dest='dir', default='DESC', help='the direction to sort the results --dir=\'ASC\' or --dir=\'DESC\'')
     (options, args) = parser.parse_args()
 
-    # options is an object we want the dictionary value of it
-    # Some of the options need a little munging...
+    # Option is an object but we need dictionary values
     option_dict = vars(options)
     if option_dict['severity']:
         option_dict['severity'] = option_dict['severity'].split(',')
     if option_dict['eventState']:
         option_dict['eventState'] = option_dict['eventState'].split(',')
 
-    # option_dict includes the sort and dir keys (as we have defaulted them in optparse)
-    # These are not part of the filter string so we need to pop them out of the dictionary
-    # to use separately.
-    s = option_dict.pop('sort')
-    d = option_dict.pop('dir')
+    # Present by default, sort and dir need removed as they are not a part of the filter string.
+    sort_key = option_dict.pop('sort')
+    sort_direction = option_dict.pop('dir')
 
-    # Need to check these keys for sanity and provide sensible defaults otherwise
-    dirlist=['ASC','DESC']
-    if not d in dirlist:
-        d='DESC'
-    sortlist = ['severity', 'eventState', 'eventClass',
-                'firstTime', 'lastTime', 'stateChange',
-                'count', 'device', 'component', 'agent',
-                'monitor']
-
-    if not s in sortlist:
-        s='lastTime'
-
+    # Create a new zenoss object and fetch its events
     zenoss = Zenoss()
-    out = zenoss.get_events(filter=option_dict, sort=s, dir=d)
+    z = zenoss.get_events(filter=option_dict, sort=sort_key, dir=sort_direction)
 
-    for e in out['events']:
+    for e in z['events']:
+        outEvid = e['evid']
         outState=e['eventState']
-        if e['DeviceClass']:
-           outDeviceClass=e['DeviceClass'][0]['name']
-        else: outDeviceClass=[]
-        outcount=e['count']
         outdevice=e['device']['text']
-        if e['Location']:
-            outLocation=e['Location'][0]['name']
-        else: outLocation=[]
-        outSystems=[]
-        for pos,val in enumerate(e['Systems']):
-            sy=str(e['Systems'][pos]['name'])
-            outSystems.append(sy)
         outseverity=e['severity']
         outfirstTime=e['firstTime']
         outlastTime=e['lastTime']
-        outsummary=e['summary']
-        print '%s,%s,%s,%s,%s,%s,%s,%s,%s,%s' % (outState, outDeviceClass, outcount, outdevice,
-                                                 outLocation, outSystems, outseverity, outfirstTime,
-                                                 outlastTime, outsummary)
+        print 'Evid: %s ,State: %s, Device: %s, Severity: %s, firstTime: %s, lastTime: %s' % (outEvid, outState, outdevice,
+                                           outseverity, outfirstTime,
+                                           outlastTime)
 
 if __name__ == "__main__":
     main()
