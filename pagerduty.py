3e2ce9bf5043e846671b86c7aa08b844d590371c
Shayne Huddleston <shayne.huddleston@oregonstate.edu>
2013-03-24 01:18:56 -0700

    closes #2 added options and usage to pagerduty

--- pagerduty.py
+++ pagerduty.py
@@ -1,20 +1,30 @@
 import json
 import requests
 from optparse import OptionParser
-from config import pd_domain, pd_service, pd_token
+from config import pagerduty_domain, pagerduty_service, pagerduty_token
 
 def main():
 
-    url = 'https://' + pd_domain + '.pagerduty.com/api/v1/incidents'
-    payload = {'fields': 'incident_key',
-               'status': 'resolved',
-               'limit': '5',
-               'offset': '0',
-               'service': pd_service,
-               'sort_by': 'resolved_on:desc'}
+    usage = 'python %prog --fields=fields --status=status  --limit=limit --offset=offset --sort_by=sort_by'
+
+    parser = OptionParser(usage)
+    parser.add_option("--fields", dest='fields', default='incident_key', help='')
+    parser.add_option("--status", dest='status',default='resolved', help='')
+    parser.add_option("--limit", dest='limit',default='5', help='')
+    parser.add_option("--offset", dest='offset',default='0', help='')
+    parser.add_option("--sort_by", dest='sort_by', default='resolved_on:desc', help='')
+    (options, args) = parser.parse_args()
+
+    url = 'https://' + pagerduty_domain + '.pagerduty.com/api/v1/incidents'
+    payload = {'fields': options.fields,
+               'status': options.status,
+               'limit': options.limit,
+               'offset': options.offset,
+               'service': pagerduty_service,
+               'sort_by': options.sort_by}
 
     headers = {'content-type': 'application/json',
-               'Authorization': 'Token token=' + pd_token}
+               'Authorization': 'Token token=' + pagerduty_token}
 
     resp = requests.get(url, data=json.dumps(payload), headers=headers);
 
